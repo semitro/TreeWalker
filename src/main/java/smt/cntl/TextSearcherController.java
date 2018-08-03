3eb42f6f35010a6e0f125ad3eedf7329d760d453
Semitro <semitro_8@mail.ru>
2018-08-04 00:02:52 +0700

    directory chooser

--- src/main/java/smt/cntl/TextSearcherController.java
+++ src/main/java/smt/cntl/TextSearcherController.java
@@ -1,5 +1,17 @@
 package smt.cntl;
 
-public class TextSearcherController {
+import javafx.scene.Scene;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
+import java.io.File;
+
+public class TextSearcherController {
+    private Stage fileChooserStage = new Stage();
+    private DirectoryChooser directoryChooser = new DirectoryChooser();
+    public void onSetRootClick(){
+        directoryChooser.showDialog(null);
+    }
 }
