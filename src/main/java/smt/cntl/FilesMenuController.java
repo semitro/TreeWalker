316d1de7b38e9025fa283c1c2466a331383ee6e2
Semitro <semitro_8@mail.ru>
2018-08-05 15:28:51 +0700

    doing new controller

--- src/main/java/smt/cntl/FilesMenuController.java
+++ src/main/java/smt/cntl/FilesMenuController.java
@@ -1,4 +1,40 @@
 package smt.cntl;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import smt.util.PathsToTreeTransormer;
+
 public class FilesMenuController {
+
+    @FXML private TreeView fileHierarchy;
+    @FXML private TabPane tabPane;
+
+    @FXML
+    public void initialize(){
+        fileHierarchy.getSelectionModel().selectedItemProperty().addListener((observable,old,node)->{
+            if(((TreeItem<String>)node).isLeaf())
+                System.out.println(new PathsToTreeTransormer().leafToPath((TreeItem<String>) node));
+
+            if(tabPane.getTabs().
+                    filtered(tab->tab.getText().equals(((TreeItem<String>) node).getValue()))
+                    .isEmpty()){
+                Tab newTab = new Tab(((TreeItem<String>) node).getValue());
+                TextFlow textFlow = new TextFlow();
+                Text text1 = new Text("asf");
+                Text text2 = new Text("sff");
+                text2.setStyle("-fx-color: green");
+                text2.setStroke(Color.GREEN);
+                textFlow.getChildren().add(text1);
+                textFlow.getChildren().add(text2);
+                newTab.setContent(textFlow);
+                tabPane.getTabs().add(newTab);
+            }
+        });
+    }
 }
--- src/main/java/smt/cntl/TextSearcherController.java
+++ src/main/java/smt/cntl/TextSearcherController.java
@@ -20,43 +20,15 @@ import java.util.stream.Stream;
 
 public class TextSearcherController {
 
-    @FXML private TreeView fileHierarchy;
-    @FXML private TabPane tabPane;
     @FXML private BottomMenuController bottomMenuController;
     private FileFinder fileFinder = new FileFinder();
+
     @FXML
-    public void initialize(){
-        fileHierarchy.getSelectionModel().selectedItemProperty().addListener((observable,old,node)->{
-            if(((TreeItem<String>)node).isLeaf())
-                System.out.println(new PathsToTreeTransormer().leafToPath((TreeItem<String>) node));
-
-            if(tabPane.getTabs().
-                    filtered(tab->tab.getText().equals(((TreeItem<String>) node).getValue()))
-                    .isEmpty()){
-                Tab newTab = new Tab(((TreeItem<String>) node).getValue());
-                TextFlow textFlow = new TextFlow();
-                Text text1 = new Text("asf");
-                Text text2 = new Text("sff");
-                text2.setStyle("-fx-color: green");
-                text2.setStroke(Color.GREEN);
-                textFlow.getChildren().add(text1);
-                textFlow.getChildren().add(text2);
-                newTab.setContent(textFlow);
-                tabPane.getTabs().add(newTab);
-            }
-        });
+    public void initialize() {
+   RRR
     }
-
     public void onHierarchyClick(MouseEvent event){
         System.out.println(event.getPickResult().getIntersectedNode().toString());
-        bottomMenuController.setFindClickCallback((root, postfix, text)->{
-            try {
-                List<Path> paths = fileFinder.findFiles(root, postfix, text)
-                        .sorted().collect(Collectors.toList());
-            }catch (IOException ioe){
-                ioe.printStackTrace();
-            }
-        });
   }
 
 }
