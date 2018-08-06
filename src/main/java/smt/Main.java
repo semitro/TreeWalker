0f35b09abd2bef367e8ad61aba7f109e7d1e1fe5
Semitro <semitro_8@mail.ru>
2018-08-06 17:11:56 +0700

    new selected text color

--- src/main/java/smt/Main.java
+++ src/main/java/smt/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         final Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
-        primaryStage.setTitle("Tree walker");
+        primaryStage.setTitle("Tree walker by Oshchepkov Artem - github.com/semitro");
         final Screen screen = Screen.getPrimary();
         final Rectangle2D screenSize = screen.getBounds();
         final Scene scene = new Scene(root,screenSize.getWidth(),screenSize.getHeight());
